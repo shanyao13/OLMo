f5f7400579527333e61d90859b6ebfc48c2ec646
shanyao13 <swz_13@163.com>
2025-04-24 15:27:24 +0800

    wb设置和修改olmo参数加快训练

--- olmo/config.py
+++ olmo/config.py
@@ -691,7 +691,7 @@ class TokenizerConfig(BaseConfig):
 @dataclass
 class WandbConfig(BaseConfig):
     project: Optional[str] = None
-    entity: Optional[str] = "ai2-llm"
+    entity: Optional[str] = "zhengshui13"
     group: Optional[str] = None
     name: Optional[str] = None
     tags: Optional[List[str]] = field(default_factory=lambda: ["watching"])
